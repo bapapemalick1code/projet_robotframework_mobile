c864dd0d56866da0249f8821b3901685d8a3e8c4
bapapemalick1code <bapapemalick1@gmail.com>
2023-07-17 18:43:14 +0200

    integration nouveau dossier

---
+++ PageObjects/locators.py
@@ -0,0 +1,15 @@
+### Onboarding ###
+id_btn_demarrer=  "id=bt_start_showcase"
+id_indicator_swipe=  "id=indicator_showcase"
+id_btn_valider=  "id=bt_stop_showcase"
+id_btn_allow_notif=  "id=v_seekbar_overlay"
+id_titre_popup=  "id=tv_why_user_title"
+id_skip_popup=  "id=tv_why_skip"
+
+### ###
+id_btn_switch_mode=  "bt_switch_mode"
+id_mode_acheteur= "id=tv_mode_buyer"
+id_mode_vendeur= "id=tv_mode_seller"
+id_titre_acheteur=  "id=tv_buyer_tuto_title"
+id_titre_vendeur=  "id=tv_seller_tuto_title"
+xpath_titre_texte=  "xpath=/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.TextView"
